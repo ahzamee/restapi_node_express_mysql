0d6af81b0a000c721786b7e92e88a5c62d28a528
zamee <zamee0192@gmail.com>
2021-11-22 10:47:34 +0600

    upload image init

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const multer = require('multer');
+fs = require('fs-extra')
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
@@ -21,3 +24,30 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now()+'.jpg')
+  }
+})
+
+var upload = multer({ storage: storage })
+
+// Upload Single File
+app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    console.log("error", 'Please upload a file');
+    
+    res.send({code:500, msg:'Please upload a file'})
+    return next({code:500, msg:error})
+
+  }
+  res.send({code:200, msg:file})
+
+})
